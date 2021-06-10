514284aa7f7a6f38da10f802f50014417eccc362
anthonycc2 <anthonycc2@users.noreply.github.com>
2021-06-10 03:00:44 -0300

    Deploy no Heroku

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-//Install express server
+/*//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -12,4 +12,20 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);*/
+
+//Importa as dependências que acabamos de instalar
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + "/dist/cesarlog-frontend"));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/cesarlog-frontend/index.html"));
+});
+
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
